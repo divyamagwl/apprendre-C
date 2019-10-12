1c4dc40fcda1bb0fba66533755e8fbffcfadf7fa
stupidcoder-atom <56150881+stupidcoder-atom@users.noreply.github.com>
2019-10-12 20:32:17 +0530

    Update LAB4.1.c

--- LAB4/LAB4.1.c
+++ LAB4/LAB4.1.c
@@ -2,20 +2,13 @@
 int main(void)
 {
 
-int array[10]; //Initializing an integer array
+	int array[10]; //Initializing an integer array
 
-for (int i = 0 ; i < 10 ; i++) 
-{
-	scanf("%d,",&array[i]); //Taking input as integers separated by a comma
-}
-
-for (int i = 9; i >= 0; i--)
-{
-	printf("%d ",array[i]); //Printing integers in reverse order separated by space
-}
-
-printf("\n");
-
-return 0;
+	for (int i = 0 ; i < 10 ; i++) 
+		scanf("%d,",&array[i]); //Taking input as integers separated by a comma
+	for (int i = 9; i >= 0; i--)
+		printf("%d ",array[i]); //Printing integers in reverse order separated by space
+	printf("\n");
+	return 0;
 
 }
