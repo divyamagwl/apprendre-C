a00de90c892a39acdc2fb8513de23e5f65720313
lalith-krg <56600704+lalith-krg@users.noreply.github.com>
2019-10-15 22:41:46 +0530

    Update LAB2.2.c

--- LAB2/LAB2.2.c
+++ LAB2/LAB2.2.c
@@ -1,31 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //Not used the concept of Sieve of Eratosthenes
 //You should try using Sieve of Eratosthenes (Check Sieve.c) 
 
 int main(void)
 {
-int a, prime=0;
-scanf("%d",&a);
+	int a, prime=0;
+	scanf("%d", &a);
 
-if(a>1)
-{
-	for(int i=2; i<=a; i++)
+	if(a > 1)
 	{
-		int x=0;
-		for(int j=1; j<=i; j++)
+		for(int i=2; i <= a; i++)
 		{
-			if(i%j==0)
-			x++;
+			int check=0;
+			for(int j=1; j <= i; j++)
+			{
+				if( i % j == 0 )
+				check++;
+			}
+			if(check == 2)
+			prime++;
 		}
-		if(x==2)
-		prime++;
+		printf("%d\n", prime);
 	}
-	printf("%d\n",prime);
-}
 
-else
-	printf("0\n");
-
-return 0;
+	else
+		printf("0\n");
+	
+	return 0;
 }
